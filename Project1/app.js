271c80aca2d5179bdb26d4cde419ea341379277e
WillMarda <WillMarda@outlook.com>
2023-04-18 16:23:09 -0600

    Update app.js

--- Project1/app.js
+++ Project1/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
+// Route to Home
 app.get('/', (req, res) => {
   res.send('Willem Marda')
 })
 
+// Listen on Port 3000
 app.listen(3000, () => {
   console.log('Server listening on port 3000')
 })
